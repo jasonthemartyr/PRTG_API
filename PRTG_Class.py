fafe2e62f69706245a97bcced6f9145a04630882
jmarter <jason.marter@gmail.com>
2018-01-18 10:47:37 -0600

    general clean up, added delete method to class

--- PRTG_Class.py
+++ PRTG_Class.py
@@ -3,19 +3,18 @@ import requests
 class PRTG(object):
 
     def __init__(self, url, **kwargs):
+        payload = kwargs
         self.url = url
         self.pause ='api/pause.htm?'
-        payload = kwargs
+        self.delete = '/api/deleteobject.htm?'
+        self.username = payload.get('username') if 'username' in payload else False
+        self.password = payload.get('password') if 'password' in payload else False
+        self.id = payload.get('id') if 'id' in payload else False
+        self.pausemsg = payload.get('pausemsg') if 'pausemsg' in payload else False
 
-        for i in payload:
-            self.username = payload.get('username') if 'username' in payload else False
-            self.password = payload.get('password') if 'password' in payload else False
-            self.id = payload.get('id') if 'id' in payload else False
-            self.pausemsg = payload.get('pausemsg') if 'pausemsg' in payload else False
-
-    def modifyURL(self, uri):
+    def combine_url(self, uri):
         """
-        Modifies base URL with URI
+        Combines base URL with URI
         :param uri:
         :return:
         """
@@ -25,20 +24,26 @@ class PRTG(object):
 
     def pause_node(self):
         """
-        Pauses node
+        POST request to pause monitoring for a node
         :return:
         """
-        url = self.modifyURL(self.pause)
+        url = self.combine_url(self.pause)
         print(url)
-        request = requests.post(url, params={'username': self.username,'password': self.password,'id': self.id,  'pausemsg': self.pausemsg,'action': '0'}, verify=False)
-        return request
+        return requests.post(url, params={'username': self.username,'password': self.password,'id': self.id,  'pausemsg': self.pausemsg,'action': '0'}, verify=False)
 
     def resume_node(self):
         """
-        Resumes node
+        POST request to resume monitoring for a node
         :return:
         """
-        url = self.modifyURL(self.pause)
-        request =requests.post(url, params={'username': self.username,'password': self.password,'id': self.id, 'action': '1'}, verify=False)
-        return request
+        url = self.combine_url(self.pause)
+        return requests.post(url, params={'username': self.username,'password': self.password,'id': self.id, 'action': '1'}, verify=False)
+        #return request
 
+    def delete_node(self):
+        """
+        DELETE request to remove a node
+        :return:
+        """
+        url = self.combine_url(self.delete)
+        return requests.delete(url, params={'username': self.username,'password': self.password,'id': self.id, 'approve': '1'}, verify=False)
